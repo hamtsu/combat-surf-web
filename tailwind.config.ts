d343b10a2adfad0c8680968102944f8d90056a27
hamtsu <itseatham@gmail.com>
2024-12-12 17:30:07 +1100

    update: extend tailwind config with more animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,33 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        "bounce-right": "bounce-right 1s infinite",
+        "bounce-left": "bounce-left 1s infinite",
+        "wiggle": "wiggle 1s infinite",
+      },
+      keyframes: {
+        "bounce-right": {
+          "0%, 100%": {
+            transform: "translateX(0)",
+          },
+          "50%": {
+            transform: "translateX(2px)",
+          },
+        },
+        "bounce-left": {
+          "0%, 100%": {
+            transform: "translateX(0)",
+          },
+          "50%": {
+            transform: "translateX(-2px)",
+          },
+        },
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-6deg)' },
+          '50%': { transform: 'rotate(6deg)' },
+        }
+      }
     },
   },
   plugins: [],
