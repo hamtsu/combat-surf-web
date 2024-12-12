740c9213bda7f357ed411c25ef6eb149f35e6c9d
hamtsu <itseatham@gmail.com>
2024-12-12 17:32:08 +1100

    update: add activity heatmap to dashboard page (WIP)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,23 @@
 import UserProfile from "@/components/UserProfile";
 import mockUserInfo from "./mockUserInfo.json";
+import ActivityHeatmap from "@/components/ActivityHeatmap";
+import MinecraftCharacter from "@/components/MinecraftRenderer";
 
 export default function DashboardHome() {
   return (
-    <main className="min-h-screen p-12 bg-neutral-100">
-      <UserProfile username={mockUserInfo.username} uuid={mockUserInfo.uuid} rank={mockUserInfo.rank} groups={mockUserInfo.groups} />
+    <main className="h-full">
+      <header className="px-12 pt-24 pb-8 bg-stone-100 w-full h-full">
+        <UserProfile
+          username={mockUserInfo.username}
+          uuid={mockUserInfo.uuid}
+          rank={mockUserInfo.rank}
+          groups={mockUserInfo.groups}
+        />
+      </header>
+
+      <section className="bg-stone-200 px-10 py-4 h-full">
+        <ActivityHeatmap />
+      </section>
     </main>
   );
 }
