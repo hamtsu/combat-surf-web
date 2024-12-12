fe4b264c5bf2451d3252ce380d54dc2981a6bd67
hamtsu <itseatham@gmail.com>
2024-12-12 17:34:07 +1100

    update: user profile component (implement 3d animated skin)

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import React, { FC } from "react";
 import UserProfileRank from "./UserProfileRank";
 import UserProfileGroups from "./UserProfileGroups";
+import CopyButton from "./CopyButton";
+import MinecraftRenderer from "./MinecraftRenderer";
 
 type UserProfileProps = {
   username: string;
@@ -10,37 +12,40 @@ type UserProfileProps = {
   groups: string[];
 };
 
-const UserProfile: FC<UserProfileProps> = ({ username, rank, uuid, groups }) => {
+const SKIN_REQUEST_URL =
+  "https://skins.mcstats.com/body/front/<uuid>?scale=2&fov=50&shadow=true&disableCosmeticType=all&fallbackTexture=steve&overlay=true&cropMeasurement=pixels&expandMeasurement=pixels&cropLeft=0&cropRight=0&cropTop=0&cropBottom=350&expandLeft=0&expandRight=0&expandTop=0&expandBottom=0&alwaysSquare=false&grayscale=false";
+
+const UserProfile: FC<UserProfileProps> = ({
+  username,
+  rank,
+  uuid,
+  groups,
+}) => {
+
   return (
     <div className="w-fit flex gap-2 flex-col">
-      <div className="flex flex-col items-center justify-center p-3 px-4 bg-neutral-50 border-neutral-200 border-b-4 border-r-4 rounded-lg">
-        <h1 className="text-2xl text-neutral-900">
-          <b>{username}</b>
-        </h1>
-      </div>
-
-      <div className="flex flex-col items-center justify-center p-6 px-14 bg-neutral-50 border-neutral-200 border-b-4 border-r-4 rounded-lg">
-        <Image
-          width={150}
-          height={400}
-        //   src={`https://api.mineatar.io/body/full/${uuid}?scale=40`}
+      <div className="flex flex-row items-center justify-center bg-neutral-50 border-neutral-200 border-b-4 border-r-4 rounded-lg pl-6 pr-8">
+        {/* <Image
+          width={448}
+          height={724}
+          src={SKIN_REQUEST_URL.replace("<uuid>", uuid)}
+          className="w-[300px] overflow-hidden mt-[-80px]"
+          draggable={false}
           alt="Avatar failed to load"
-        />
+        /> */}
+
+        <MinecraftRenderer uuid={uuid} username={username} />
+
+        <div className="flex flex-col p-3">
+          <h1 className="text-4xl font-bold">{username}</h1>
+          <p className="text-neutral-500">{uuid}</p>
+          <CopyButton text={uuid} />
+        </div>
       </div>
-
+      
+      <div className="flex flex-row gap-2">
       <UserProfileRank rank={rank} />
-
-      {/* <UserProfileGroups groups={groups} /> */}
-
-      <div className="p-3 bg-neutral-200 rounded-md text-neutral-500 flex flex-col">
-        <div className="flex flex-col bg-neutral-100 p-1 px-2 pt-2 rounded-md">
-            <h3 className="text-green-500 text-lg animate-pulse font-bold leading-3 ">Online » as-practice</h3>
-            <p className="leading-1 opacity-70">last seen 2 hours ago</p>
-        </div>
-
-        <div className="flex flex-col bg-neutral-100 p-1 px-2 pt-2 rounded-md">
-            f
-        </div>
+      <UserProfileGroups groups={groups} />
       </div>
     </div>
   );
