aa84f2d4b3068e29400e866d991f369f9bfbf790
hamtsu <itseatham@gmail.com>
2024-12-12 18:31:51 +1100

    update: added buttons to change behaviour of skin renderer

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
-import React, { FC } from "react";
+"use client";
+import React, { FC, useState } from "react";
 import UserProfileRank from "./UserProfileRank";
 import UserProfileGroups from "./UserProfileGroups";
 import CopyButton from "./CopyButton";
 import MinecraftRenderer from "./MinecraftRenderer";
+import { FaMagnifyingGlass, FaPerson, FaPersonRunning, FaPersonWalking } from "react-icons/fa6";
+import Button from "./Button";
 
 type UserProfileProps = {
   username: string;
@@ -12,40 +14,65 @@ type UserProfileProps = {
   groups: string[];
 };
 
-const SKIN_REQUEST_URL =
-  "https://skins.mcstats.com/body/front/<uuid>?scale=2&fov=50&shadow=true&disableCosmeticType=all&fallbackTexture=steve&overlay=true&cropMeasurement=pixels&expandMeasurement=pixels&cropLeft=0&cropRight=0&cropTop=0&cropBottom=350&expandLeft=0&expandRight=0&expandTop=0&expandBottom=0&alwaysSquare=false&grayscale=false";
-
 const UserProfile: FC<UserProfileProps> = ({
   username,
   rank,
   uuid,
   groups,
 }) => {
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
+  const [zoomEnabled, setZoomEnabled] = useState(false);
 
   return (
     <div className="w-fit flex gap-2 flex-col">
-      <div className="flex flex-row items-center justify-center bg-neutral-50 border-neutral-200 border-b-4 border-r-4 rounded-lg pl-6 pr-8">
-        {/* <Image
-          width={448}
-          height={724}
-          src={SKIN_REQUEST_URL.replace("<uuid>", uuid)}
-          className="w-[300px] overflow-hidden mt-[-80px]"
-          draggable={false}
-          alt="Avatar failed to load"
-        /> */}
+      <div className="flex flex-row pt-10 items-center justify-center bg-neutral-50 border-neutral-200 border-b-4 border-r-4 rounded-lg pl-6 pr-8">
 
-        <MinecraftRenderer uuid={uuid} username={username} />
+        <MinecraftRenderer
+          uuid={uuid}
+          username={username}
+          rank={rank}
+          currentAnimation={currentAnimation}
+          zoomEnabled={zoomEnabled}
+        />
 
         <div className="flex flex-col p-3">
-          <h1 className="text-4xl font-bold">{username}</h1>
+          <span className="flex gap-1 items-end"><h1 className="text-4xl font-bold">{username}</h1><CopyButton text={uuid} /></span>
           <p className="text-neutral-500">{uuid}</p>
-          <CopyButton text={uuid} />
+          <div className="flex gap-1 text-2xl text-stone-800 pt-4">
+            <Button
+              className="bg-stone-200 hover:bg-black/20 p-2 group"
+              onClick={() => setCurrentAnimation("idle")}
+            >
+              <FaPerson className="group-hover:animate-wiggle" />
+            </Button>
+
+            <Button
+              className="bg-stone-200 hover:bg-black/20 p-2 group"
+              onClick={() => setCurrentAnimation("walk")}
+            >
+              <FaPersonWalking className="group-hover:animate-wiggle" />
+            </Button>
+
+            <Button
+              className="bg-stone-200 hover:bg-black/20 p-2 group"
+              onClick={() => setCurrentAnimation("run")}
+            >
+              <FaPersonRunning className="group-hover:animate-wiggle" />
+            </Button>
+
+            <Button
+              className={`${zoomEnabled? "bg-green-500 hover:bg-green-600" : "bg-stone-200 hover:bg-black/20"} p-2 group`}
+              onClick={() => setZoomEnabled(!zoomEnabled)}
+            >
+              <FaMagnifyingGlass className={`${zoomEnabled ? "animate-pulse" : 'group-hover:animate-wiggle'}`}/>
+            </Button>
+          </div>
         </div>
       </div>
-      
+
       <div className="flex flex-row gap-2">
-      <UserProfileRank rank={rank} />
-      <UserProfileGroups groups={groups} />
+        <UserProfileRank rank={rank} />
+        <UserProfileGroups groups={groups} />
       </div>
     </div>
   );
