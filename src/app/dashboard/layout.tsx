cb9a14ec9cb9730a4c37ea35fa3e13c4a51ff755
hamtsu <itseatham@gmail.com>
2024-12-13 18:37:08 +1100

    update: titlebar into component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,43 +1,25 @@
-import UserButton from "@/components/UserButton";
 import type { Metadata } from "next";
 import mockUserInfo from "./mockUserInfo.json";
-import ThemeSwitcher from "@/components/ThemeSwitcher";
-import { FaLock, FaSignOutAlt } from "react-icons/fa";
-import Button from "@/components/Button";
-import Tooltip from "@/components/Tooltip";
+import Titlebar from "@/components/Titlebar";
+import Sidebar from "@/components/Sidebar";
 
 export const metadata: Metadata = {
   title: "Dashboard",
-  description: "Home of the dashboard",
+  description: "Dashboard home",
 };
 
-export default function RootLayout({
+export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <div className="h-full">
-      <div className="fixed top-0 right-0 p-2 z-10 w-fit flex flex-row items-center gap-2 bg-neutral-50 border-neutral-200 border-b-2 border-r-2 m-2 rounded-xl">
-        <UserButton uuid={mockUserInfo.uuid} />
-        <Tooltip text="Toggle theme" position="bottom">
-          <ThemeSwitcher />
-        </Tooltip>
-
-        <Tooltip text="Lock sidebar" position="bottom">
-          <Button className="p-2 px-3 w-10 h-10 hover:bg-black/20 transition-colors rounded-lg text-stone-800">
-            <FaLock />
-          </Button>
-        </Tooltip>
-
-        <Tooltip text="Logout" position="bottom">
-          <Button className="group p-2 px-3 w-10 h-10 bg-red-500 hover:bg-red-400 transition-colors rounded-lg text-stone-100">
-            <FaSignOutAlt className="group-hover:animate-bounce-right"/>
-          </Button>
-        </Tooltip>
-      </div>
-
-      {children}
+      <Titlebar uuid={mockUserInfo.uuid} />
+      <section className="flex flex-row h-full">
+        {children}
+        <Sidebar />
+      </section>
     </div>
   );
 }
