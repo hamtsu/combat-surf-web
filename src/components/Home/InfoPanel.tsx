9d45566817cf2d312669dd9d66a8956e61d96b43
hamtsu <itseatham@gmail.com>
2025-04-27 01:44:16 +1000

    add: gavel animation to rules button

--- src/components/Home/InfoPanel.tsx
+++ src/components/Home/InfoPanel.tsx
@@ -19,14 +19,14 @@ const InfoPanel = () => {
 
       <Button
         onClick={() => router.push("/")}
-        className="px-3 py-2 h-fit flex mt-auto bg-stone-200 hover:bg-red-400 text-stone-800 hover:text-stone-200 text-lg font-sans font-bold transition-colors rounded-lg group"
+        className="px-3 py-2 h-fit flex mt-auto bg-stone-900 hover:bg-red-500 text-stone-500 hover:text-stone-200 text-lg font-sans font-bold transition-colors rounded-lg group"
       >
-        <FaGavel size={15} className="mr-2" />
+        <FaGavel size={15} className="mr-2 group-hover:animate-gavel" />
         <p className="">View rules</p>
       </Button>
       <Button
         onClick={() => router.push("/")}
-        className="px-3 py-2 h-fit flex bg-stone-200 hover:bg-stone-400 text-stone-800 text-lg font-sans font-bold transition-colors rounded-lg group"
+        className="px-3 py-2 h-fit flex bg-stone-900 hover:bg-stone-400 text-stone-500 text-lg font-sans font-bold transition-colors rounded-lg group"
       >
         <FaExternalLinkAlt size={15} className="mr-2" />
         <p className="">View item list</p>
