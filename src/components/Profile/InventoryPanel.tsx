eb98e4c38cca1a77ff33daba936cc56a50b54666
hamtsu <itseatham@gmail.com>
2025-05-04 01:14:55 +1000

    update: empty inventory check

--- src/components/Profile/InventoryPanel.tsx
+++ src/components/Profile/InventoryPanel.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import React, { FC } from "react";
+import { FaExclamationTriangle } from "react-icons/fa";
 import { FaBoxArchive } from "react-icons/fa6";
 
 type InventoryPanelProps = {
@@ -26,7 +26,7 @@ const InventoryPanel: FC<InventoryPanelProps> = ({ inventory }) => {
         <h1 className="text-4xl font-bold text-stone-400 mt-1">Player Inventory</h1>
       </div>
       <div className="grid grid-cols-5 gap-4 scroll-auto overflow-y-scroll w-fit pr-2 max-h-[330px]">
-        {inventory.map((item, index) => (
+        {inventory.length ? (inventory.map((item, index) => (
           <div
             key={index}
             className="bg-[url(/awp.png)] bg-stone-900 bg-center bg-no-repeat p-3 rounded-lg flex flex-col gap-2 w-full h-[100px] shadow-2xl"
@@ -34,7 +34,12 @@ const InventoryPanel: FC<InventoryPanelProps> = ({ inventory }) => {
             <h2 className="text-xl font-bold text-stone-300 text-shadow">{item.name}</h2>
             <p className="text-lg font-normal text-stone-400 text-shadow">{item.type}</p>
           </div>
-        ))}
+        ))) : (
+          <div className="flex flex-col items-center justify-center gap-2 w-full h-[100px] bg-stone-900 rounded-lg p-3 shadow-2xl">
+            <FaExclamationTriangle size={35} className="fill-stone-600" />
+            <h2 className="text-xl font-bold text-stone-300 text-shadow">No items in inventory</h2>
+          </div>
+        )}
       </div>
     </div>
   );
