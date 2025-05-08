fd3447a087fe789040322cc2ce2c90935e0e5acb
hamtsu <itseatham@gmail.com>
2025-05-08 23:25:20 +1000

    update: tooltip z index to fix overlaying

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -11,7 +11,7 @@ const Tooltip: FC<TooltipProps> = ({ text, position, children }) => {
     return (
       <div className="relative flex items-center group w-fit">
         {children}
-        <div className="absolute top-full mb-2 mt-2 hidden group-hover:flex items-center justify-center p-2 bg-stone-800 text-stone-100 w-max text-sm rounded-md shadow-lg -translate-x-1/2 left-1/2">
+        <div className="absolute top-full z-[10] mb-2 mt-2 hidden group-hover:flex items-center justify-center p-2 bg-stone-800 text-stone-100 w-max text-sm rounded-md shadow-lg -translate-x-1/2 left-1/2">
           {text}
           <div className="absolute top-0 -mt-1 h-2 w-2 bg-stone-800 rotate-45" />
         </div>
@@ -21,7 +21,7 @@ const Tooltip: FC<TooltipProps> = ({ text, position, children }) => {
     return (
         <div className="relative flex items-center group w-fit">
           {children}
-          <div className="absolute bottom-full mb-4 hidden group-hover:flex items-center justify-center p-2 bg-stone-800 text-stone-100 w-max text-sm rounded-md shadow-lg -translate-x-1/2 left-1/2">
+          <div className="absolute z-[10] bottom-full mb-4 hidden group-hover:flex items-center justify-center p-2 bg-stone-800 text-stone-100 w-max text-sm rounded-md shadow-lg -translate-x-1/2 left-1/2">
             {text}
             <div className="absolute -mt-1 top-full h-2 w-2 bg-stone-800 rotate-45" />
           </div>
