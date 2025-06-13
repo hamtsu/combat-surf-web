e64449dc06b01735f0103cb5a04dc425be31ce77
hamtsu <itseatham@gmail.com>
2025-06-13 23:08:16 +1000

    update: info panel button links to reflect new rules and itemlist pages

--- src/components/Home/InfoPanel.tsx
+++ src/components/Home/InfoPanel.tsx
@@ -8,24 +8,24 @@ const InfoPanel = () => {
   const router = useRouter();
 
   return (
-    <div className="h-full bg-stone-800 p-4 rounded-md flex flex-col gap-3">
+    <div className="bg-stone-800 p-4 rounded-md h-full flex flex-col gap-3">
       <h1 className="text-2xl font-bold text-stone-300">
         What is <b>Combat Surf</b>?
       </h1>
-      <p className="text-stone-200/80">
+      <p className="text-stone-200/80 mb-6">
         Combat Surf is a roblox game based off of the classic community gamemode from the Counter-Strike series! It is a fast paced movement shooter which requires knowledge of
         surfing and strafing.
       </p>
 
       <Button
-        onClick={() => router.push("/")}
+        onClick={() => router.push("/rules")}
         className="px-3 py-2 h-fit flex mt-auto bg-stone-900 hover:bg-red-500 text-stone-500 hover:text-stone-200 text-lg font-sans font-bold transition-colors rounded-lg group"
       >
         <FaGavel size={15} className="mr-2 group-hover:animate-gavel" />
         <p className="">View rules</p>
       </Button>
       <Button
-        onClick={() => router.push("/")}
+        onClick={() => router.push("/item-list")}
         className="px-3 py-2 h-fit flex bg-stone-900 hover:bg-stone-400 text-stone-500 hover:text-stone-800 text-lg font-sans font-bold transition-colors rounded-lg group"
       >
         <FaExternalLinkAlt size={15} className="mr-2" />
