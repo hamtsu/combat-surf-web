32579e40e1fbce098fa30ab83d91c455d4c652b2
Hammy <68724706+hamtsu@users.noreply.github.com>
2025-07-16 15:29:41 +1000

    update: middleware to allow more requests for specific endpoints

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,32 +7,58 @@ type RateLimitEntry = {
 
 const rateLimitMap = new Map<string, RateLimitEntry>();
 
-const MAX_REQUESTS = 50
-const WINDOW_MS = 60 * 1000 
+const MAX_REQUESTS = 15; // 15 req per minute
+const WINDOW_MS = 60 * 1000;
 
 function getClientIp(req: NextRequest): string {
-  return req.ip ?? req.headers.get("x-forwarded-for")?.split(",")[0] ?? "unknown"
+  return (
+    req.ip ?? req.headers.get("x-forwarded-for")?.split(",")[0] ?? "unknown"
+  );
 }
 
 export function middleware(req: NextRequest) {
-  const ip = getClientIp(req)
-  const now = Date.now()
+  const ip = getClientIp(req);
+  const now = Date.now();
 
-  const entry = rateLimitMap.get(ip)
+  const entry = rateLimitMap.get(ip);
 
   if (!entry) {
-    rateLimitMap.set(ip, { count: 1, lastReset: now })
-    return NextResponse.next()
+    rateLimitMap.set(ip, { count: 1, lastReset: now });
+    return NextResponse.next();
   }
 
   if (now - entry.lastReset > WINDOW_MS) {
     // reset time
-    rateLimitMap.set(ip, { count: 1, lastReset: now })
-    return NextResponse.next()
+    rateLimitMap.set(ip, { count: 1, lastReset: now });
+    return NextResponse.next();
   }
 
-  if (entry.count >= MAX_REQUESTS) {
-    return NextResponse.json({ message: "Too many requests. Try again later"}, { status: 429 })
+  const path = req.nextUrl.pathname;
+  console.warn(path);
+  if (
+    path == "/api/player-rank" ||
+    path == "/api/game-info" ||
+    path == "/api/avatar" ||
+    path == "/api/changelogs"
+  ) {
+    // allow more requests for these endpoints
+    if (entry.count >= MAX_REQUESTS + 10) {
+      return NextResponse.json(
+        {
+          message: "Too many requests. Try again later",
+         // waitSeconds: 60 - Math.round((now - entry.lastReset) / 1000),
+        },
+        { status: 429 }
+      );
+    }
+  } else if (entry.count >= MAX_REQUESTS) {
+    return NextResponse.json(
+      {
+        message: "Too many requests. Try again later",
+        // waitSeconds: 60 - Math.round((now - entry.lastReset) / 1000),
+      },
+      { status: 429 }
+    );
   }
 
   entry.count++;
@@ -40,5 +66,14 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/api/:path*"],
-}
+  matcher: [
+    "/api/player-info",
+    "/api/clan-info",
+    "/api/changelogs",
+    "/api/leaderboard",
+    "/api/player-rank",
+    "/api/game-info",
+    "/api/avatar",
+    "/item-list",
+  ],
+};
