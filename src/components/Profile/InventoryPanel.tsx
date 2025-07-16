787e97979f7a01c031b6b42d27e7252d6498fed3
hamtsu <itseatham@gmail.com>
2025-07-17 01:14:17 +1000

    update: redesign inventory

--- src/components/Profile/InventoryPanel.tsx
+++ src/components/Profile/InventoryPanel.tsx
@@ -1,59 +1,144 @@
-import React, { FC } from "react";
-import { FaExclamationTriangle } from "react-icons/fa";
+"use client";
+import React, { FC, useState } from "react";
+import { FaCaretRight, FaExclamationTriangle, FaTag } from "react-icons/fa";
 import { FaBoxArchive } from "react-icons/fa6";
 
 type InventoryPanelProps = {
-  inventory:
-    | [
-        {
-          id: number;
-          name: string;
-          type: string;
-          skin: string;
-          rarity: string;
-          value: number;
-          imageUrl: string;
-        }
-      ]
-    | [];
+  inventory: {
+    inventory: {
+      [key: string]: {
+        Name: string;
+        SubType?: string;
+        Base: string;
+        Rarity: number;
+      };
+    };
+  };
+  onItemClick: (item: any) => void;
 };
 
-const InventoryPanel: FC<InventoryPanelProps> = ({ inventory }) => {
+const InventoryPanel: FC<InventoryPanelProps> = ({
+  inventory,
+  onItemClick,
+}) => {
   const sortedItems = Object.entries(inventory.inventory).sort(
     (a, b) => b[1].Rarity - a[1].Rarity
   );
-  
+
+  const [isFirstLoad, setIsFirstLoad] = useState<boolean>(true);
+  const [filteredItems, setFilteredItems] = useState<any[]>(sortedItems);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsFirstLoad(false);
+
+    const searchTerm = event.target.value.toLowerCase();
+
+    if (searchTerm.length === 0) {
+      setFilteredItems(sortedItems);
+      return;
+    }
+
+    setFilteredItems(
+      sortedItems.filter((item) =>
+        item[1].Name.toLowerCase().includes(searchTerm)
+      )
+    );
+  };
+
   return (
     <div className="flex flex-col gap-3 bg-stone-800 pl-4 py-4 pr-2 rounded-lg w-full animate-fade-in-sixth opacity-0">
       <div className="flex gap-4">
         <div className="p-2 bg-stone-900 rounded-md h-fit">
           <FaBoxArchive size={35} className="fill-stone-600" />
         </div>
-        <h1 className="text-4xl font-bold text-stone-400 mt-1">
-          Player Inventory
-        </h1>
+        <h1 className="text-4xl font-bold text-stone-400 mt-1">Inventory</h1>
+
+        <input
+          type="text"
+          name="item-search"
+          className="bg-stone-900 ml-6 rounded-md p-2 my-auto h-fit px-3 text-stone-200 w-[250px]"
+          placeholder="Filter Items..."
+          onChange={handleSearch}
+          autoComplete="off"
+          autoCorrect="off"
+        />
+
+        <div className="bg-stone-900 my-auto p-1 px-2 h-fit text-sm rounded-sm font-mono text-stone-300 opacity-70">
+          {filteredItems.length} items
+        </div>
       </div>
-      <div className="grid grid-cols-5 gap-4 scroll-auto overflow-y-scroll w-fit pr-2 max-h-[330px]">
-        {inventory ? (
-          sortedItems.map(([id, item]) => (
+      <div className="grid grid-cols-4 gap-4 scroll-auto overflow-y-scroll w-fit pr-2 max-h-[330px] max-w-[1300px]">
+        {inventory && filteredItems.length > 0 ? (
+          filteredItems.map(([id, item], index) => (
             <div
               key={id}
-              style={{ backgroundImage: `url(/items/${item.SubType ?? item.Name}.png)` }}
-              className={`bg-stone-900 bg-top bg-no-repeat bg-cover p-3 rounded-lg flex flex-col w-full h-[92px] shadow-2xl`}
+              style={
+                isFirstLoad
+                  ? {
+                      animationDelay: `${index * 0.1 + 3}s`,
+                      opacity: 0,
+                      backgroundImage: `url(/items/${
+                        item.SubType ?? item.Name
+                      }.png)`,
+                    }
+                  : {
+                      backgroundImage: `url(/items/${
+                        item.SubType ?? item.Name
+                      }.png)`,
+                    }
+              }
+              onClick={() => onItemClick(item)}
+              className={`hover:cursor-pointer hover:shadow-2xl group animate-fade-in overflow-hidden bg-stone-900 bg-top bg-no-repeat bg-cover rounded-lg flex flex-col w-[250px] h-[110px] shadow`}
             >
-              <h2 className="text-lg font-bold text-stone-300 text-shadow">
-                {item.Name.replaceAll("_", " ")}
+              <h2 className="text-lg font-bold p-3 text-stone-300 text-shadow-md">
+                {item.Name.includes("Kill Counter")
+                  ? item.Name.replace("| Kill Counter", "").replaceAll("_", " ")
+                  : item.Name.replaceAll("_", " ")}
               </h2>
-              <p className="font-normal text-stone-400 text-shadow">
-                {item.Base}
-              </p>
+
+              <div className="px-2 bg-stone-900 py-1 mt-auto">
+                <div className="group-hover:hidden h-full flex items-center gap-1">
+                  <span className="text-stone-400 text-sm">
+                    {item.Base ? item.Base.replaceAll("_", " ") : ""}
+                  </span>
+                  <span className="text-stone-500 text-xs ml-2">
+                    rarity: {item.Rarity}
+                  </span>
+                  <div className="flex gap-1 ml-auto">
+                    {(item.SubType
+                      ? item.Name !== item.SubType
+                      : item.Name !== item.Base) &&
+                      !item.Name.includes("| Kill Counter") && (
+                        <div className="bg-stone-800 ml-auto p-1 flex items-center gap-1 text-xs rounded-md">
+                          <FaTag size={10} className="fill-stone-500" />
+                        </div>
+                      )}
+                    {item.StatTrak && (
+                      <div className="bg-stone-800 ml-auto p-[2px] px-1 flex items-center gap-1 text-xs rounded-md ">
+                        <FaExclamationTriangle
+                          size={10}
+                          className="fill-stone-500"
+                        />
+                        <span className="text-stone-500">Kill Counter</span>
+                      </div>
+                    )}
+                  </div>
+                </div>
+
+                <div className="group-hover:flex hidden gap-1 items-center">
+                  <FaCaretRight size={20} className="fill-stone-500" />
+                  <span className="text-stone-200 text-sm lowercase tracking-wider animate-pulse">
+                    view details
+                  </span>
+                </div>
+              </div>
             </div>
           ))
         ) : (
-          <div className="flex flex-col items-center justify-center gap-2 w-full h-[100px] bg-stone-900 rounded-lg p-3 shadow-2xl">
+          <div className="flex flex-col items-center justify-center gap-2 w-full h-full bg-stone-900 rounded-lg p-3 shadow-2xl">
             <FaExclamationTriangle size={35} className="fill-stone-600" />
-            <h2 className="text-xl font-bold text-stone-300 text-shadow">
-              Failed to load inventory!
+            <h2 className="text-xl font-bold text-stone-300 text-shadow ">
+              No items found!
             </h2>
           </div>
         )}
