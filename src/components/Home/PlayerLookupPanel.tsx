4e3c84e9b13362a71e4835013505ef22e4c66364
hamtsu <itseatham@gmail.com>
2025-07-21 22:24:58 +1000

    update: remove unnecessary imports from player lookup panel

--- src/components/Home/PlayerLookupPanel.tsx
+++ src/components/Home/PlayerLookupPanel.tsx
@@ -1,13 +1,8 @@
-"use client";
-
 import React from "react";
-import { useRouter } from "next/navigation";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import Searchbar from "../Searchbar";
 
 const PlayerLookupPanel = () => {
-  const router = useRouter();
-
   return (
     <div className="h-fit bg-stone-800 p-4 rounded-md flex flex-col gap-3 w-full md:w-fit">
       <div className="flex gap-3">
