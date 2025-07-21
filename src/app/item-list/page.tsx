571538a9cdeb11cb3ee9aac73e61ff32636af49e
hamtsu <itseatham@gmail.com>
2025-07-21 22:21:33 +1000

    remove: old wip notice from item list

--- src/app/item-list/page.tsx
+++ src/app/item-list/page.tsx
@@ -58,12 +58,6 @@ const Page = () => {
   return (
     <div className="overflow-y-scroll h-full flex flex-col items-center pb-24">
       <div className="flex flex-col gap-4 my-4 h-full mt-[120px] mb-[400px] px-16">
-        {/* <div className="py-5 flex flex-col gap-6 text-5xl h-fit my-auto font-black text-amber-300">
-            <div className="h-[30px] w-full bg-construction"/>
-            <h1>Under construction!</h1>
-            <p className="text-3xl">(ᗒᗣᗕ)՞</p>
-            <div className="text-xl flex gap-3 items-center opacity-50 hover:opacity-100 w-fit font-normal text-stone-200 cursor-pointer" onClick={() => router.push("/")}><FaArrowLeft /> Go back to site</div>
-        </div> */}
 
         <ItemsListHeader />
 
