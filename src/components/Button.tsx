a7a6859021f9fece8501f1dd10172cdade4a50a1
hamtsu <itseatham@gmail.com>
2026-01-30 15:56:47 +1100

    update: button styling to include custom style prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,12 +3,13 @@ import React, { Children, FC } from 'react'
 type ButtonProps = {
     className: string,
     children: React.ReactNode,
-    onClick?: () => void
+    onClick?: () => void,
+    style ?: React.CSSProperties,
 }
 
-const Button: FC<ButtonProps> = ({ className, children, onClick }) => {
+const Button: FC<ButtonProps> = ({ className, children, onClick, style }) => {
   return (
-    <button onClick={onClick} className={"rounded-md flex items-center gap-1 select-none hover:cursor-pointer " + className}>
+    <button style={style} onClick={onClick} className={"rounded-md flex items-center gap-1 select-none hover:cursor-pointer " + className}>
         {children}
     </button>
   )
